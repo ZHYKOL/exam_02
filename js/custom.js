b666ab26e10e2cb181d2c885983ae45d5661ae3a
ZHYKOL <kapeigood@gmail.com>
2017-11-26 20:12:19 +0200

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+		$('.portfolio-list').masonry({
+			// options
+			itemSelector: '.portfolio-list__item',
+			columnWidth: '.portfolio-list__item',
+			gutter: 5
+		
+		});
+});
---
+++ js/js.js
@@ -0,0 +1,8 @@
+$('.team-slide').slick({
+	cssEase: 'ease-in',
+	arrows: false,
+	dots: true,
+	 autoplay: true,
+	  autoplaySpeed: 4000,
+});
+
